082ffe7899d892d8c85c64b62823c7c279fb678a
tshinhar <tshinhar@gmail.com>
2023-02-17 15:53:05 +0200

    Create winget_fresh_system.ps1

---
+++ powershell_scripts/winget_fresh_system.ps1
@@ -0,0 +1,26 @@
+<# Installing some common applications 
+than checking for updates
+you should comment any application that you don't want to install
+#>
+
+
+winget install -e --id=Valve.Steam;
+winget install -e --id=Google.Chrome; 
+winget install -e --id=ElectronicArts.EADesktop;
+winget install -e --id=TIDALMusicAS.TIDAL;
+winget install -e --id=GOG.Galaxy;
+winget install -e --id=PeterPawlowski.foobar2000;
+winget install -e --id 7zip.7zip;
+winget install -e --id Discord.Discord;
+winget install -e --id Docker.DockerDesktop;
+winget install -e --id Git.Git;
+winget install -e --id Intel.IntelDriverAndSupportAssistant;
+winget install -e --id KhronosGroup.VulkanSDK;
+winget install -e --id LIGHTNINGUK.ImgBurn;
+winget install -e --id Microsoft.VisualStudio.Community;
+winget install -e --id Microsoft.VisualStudioCode;
+winget install -e --id Microsoft.WindowsTerminal;
+winget install -e --id Mozilla.Firefox.DeveloperEdition;
+winget install -e --id TechPowerUp.GPU-Z;
+winget install -e --id REALiX.HWiNFO;
+winget install -e --id Mozilla.Firefox.DeveloperEdition;
