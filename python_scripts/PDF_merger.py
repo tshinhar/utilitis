a7a89edf8dd766cd131eeb8ee2ce17279969f69d
tshinhar <tshinhar@gmail.com>
2023-02-17 16:07:48 +0200

    Create PDF_merger.py

---
+++ python_scripts/PDF_merger.py
@@ -0,0 +1,26 @@
+# Import the required modules
+import os
+from PyPDF2 import PdfFileMerger
+
+# Create a PdfFileMerger object
+merger = PdfFileMerger()
+
+# Ask the user for the directory containing the PDF files
+directory = input("Enter the directory containing the PDF files: ")
+
+# Get a list of all the PDF files in the directory
+pdf_files = [f for f in os.listdir(directory) if f.endswith(".pdf")]
+
+# Add each PDF file to the merger
+for pdf in pdf_files:
+    merger.append(open(pdf, 'rb'))
+
+# Ask the user for the name of the output file
+output_file = input("Enter the name of the output file: ")
+
+# Write the merged PDF to the output file
+with open(output_file, "wb") as fout:
+    merger.write(fout)
+
+# Print a success message
+print("PDF files successfully merged!")
